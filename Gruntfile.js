8fe843e405bb4b5cf83b1e2e1f6808cd00829531
Trevor Rawlings <trev_63@hotmail.com>
2017-01-01 21:57:22 -0800

    Files submitted to sculpteo

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,8 @@
+var exec = require('sync-exec'),
+_ = require('underscore');
+
 var eaglePath = (function() {
-	var exec = require('sync-exec'),
-		where = exec('where.exe eagle.exe');
+	var where = exec('where.exe eagle.exe');
 		
 	if (where.status === 0 && where.stdout.length > 0) {
 		return where.stdout.trim();
@@ -8,7 +10,17 @@ var eaglePath = (function() {
 	
 	// TODO: Eagle on windows seems to install to C:\\EAGLE-*\\bin\\eagle.exe
 	//       We could search there before giving up
-	grunt.log.error("Eagle was not found on the path");
+	console.log("Eagle was not found on the path");
+})();
+
+var freeCadPath = (function() {
+	var where = exec('where.exe FreeCAD.exe');
+		
+	if (where.status === 0 && where.stdout.length > 0) {
+		return where.stdout.trim();
+    }
+	
+	console.log("freeCad was not found on the path");
 })();
 
 function echoCommand(command) {
@@ -16,26 +28,48 @@ function echoCommand(command) {
 	return command;
 }
 
-
 var gerberFiles = ['.dri', 'GBL', 'GBO', 'GBS', 'GTL', 'GTO','GTS','TXT','dri','gpi','GML','GTP'].map((extension) => `./eagle/schematic.${extension}`);
-
+    freeCadModels = ['bottom', 'case'],
+	freeCadExportFormats = ['step', 'stl'];
 
 module.exports = function(grunt) {
+// __dirname 
 
-  grunt.initConfig({
+  var config = {
     pkg: grunt.file.readJSON('package.json'),
 	shell: {
         eagle_images: {
             command: function () { return echoCommand(eaglePath + ' ./eagle/schematic.sch -C "EXPORT image schematic.png 1000; BOARD;EXPORT image pcb.png 1000;PRINT FILE ./pcb.pdf;QUIT"'); } 
         }
+		// export_base_step
+		// export_base_stl
+		// export_case_step
+		// export_case_stl		
     },
 	clean: {
 		images: ['schematic.png', 'pcb.png', 'pcb.pdf'],
-		eagle: [].concat(gerberFiles,['./eagle/schematic.b#*','./eagle/schematic.s#*', './eagle/schematic_0*.pro','./eagle/schematic_0*.job'])
+		eagle: [].concat(gerberFiles,['./eagle/schematic.b#*','./eagle/schematic.s#*', './eagle/schematic_0*.pro','./eagle/schematic_0*.job']),
+		freeCadExports: []
 	}
+  };
+  var freeCadExportCommands = _.map(freeCadModels, function (model) {
+	  return _.map(freeCadExportFormats, function (format) {
+		  var command = 'export_' + model + '_' + format,
+		      localDir = __dirname + "/freeCad/";
+
+		  config.shell[command] = {
+			  command: function () {
+				  return echoCommand(`"${freeCadPath}" --hidden --output=${localDir}${model}.${format} ${localDir}${model}.FCStd`);
+			  }
+		  }
+		  config.clean.freeCadExports.push(`./freeCad/${model}.${format}`);
+		  return `shell:${command}`;		  
+	  });
   });
+  grunt.initConfig(config);
 
   grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.registerTask('images', ['clean:images', 'shell:eagle_images']);
+  grunt.registerTask('freeCadExports',  ['clean:freeCadExports'].concat(_.flatten(freeCadExportCommands)))
 };
